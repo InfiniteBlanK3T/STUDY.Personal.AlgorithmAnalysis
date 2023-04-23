28f53a87200ca2cb82e1ee8ddf431abdafb007fb
Thomas <vo.thomas.11647@outlook.com>
2023-04-23 19:45:15 +1000

    FisherAndYates Take decades to do

--- FisherAndYatesSorter.h
+++ FisherAndYatesSorter.h
@@ -13,15 +13,57 @@ class FisherAndYatesSorter : public BaseSorter<T>
 {
 private:
     
-    void shuffle() noexcept;
+    void shuffle() noexcept
+    {
+        size_t n = this->size();
+
+        while (n > 0)
+        {
+            size_t k = std::rand() % n;
+            n--;
+            this->swap(n, k);
+        }
+    }
 	
     template<typename C>
-    bool isSorted( C aIsOutOfOrder ) const noexcept;
+    bool isSorted(C aIsOutOfOrder) const noexcept
+    {
+        for (size_t i = 1; i < this->size(); i++)
+        {
+            if ( aIsOutOfOrder((*this)[i - 1], (*this)[i]) )
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
     
 public:
     
-    FisherAndYatesSorter( T* aCollection = nullptr, size_t aSize = 0 );
+    FisherAndYatesSorter( T* aCollection = nullptr, size_t aSize = 0 ):
+        BaseSorter<T>(aCollection, aSize)
+    {}
+
         
     template<typename C = std::greater<T>>
-    void operator()( bool aPrintStage = true, C aIsOutOfOrder = C{} ) noexcept
+    void operator()(bool aPrintStage = true, C aIsOutOfOrder = C{}) noexcept
+    {
+        bool sort = false;
+
+        while (!sort)
+        {
+            sort = this->isSorted(aIsOutOfOrder);
+
+            if (aPrintStage)
+            {
+                this->printStage(2);
+            }
+
+            if (!sort)
+            {
+                this->shuffle();
+            }
+        }
+    }
 };
